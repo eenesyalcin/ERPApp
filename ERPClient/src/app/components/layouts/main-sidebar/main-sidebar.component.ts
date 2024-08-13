3f46373d9291d12de88f473eef2b1326a39f0dea
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-08-13 15:31:56 +0300

    A structure was created to create a menu list for the main sidebar.

--- ERPClient/src/app/components/layouts/main-sidebar/main-sidebar.component.ts
+++ ERPClient/src/app/components/layouts/main-sidebar/main-sidebar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Menus } from '../../../menus';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { MenuPipe } from '../../../pipes/menu.pipe';
 
 @Component({
   selector: 'app-main-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, FormsModule, MenuPipe],
   templateUrl: './main-sidebar.component.html',
   styleUrl: './main-sidebar.component.css'
 })
 export class MainSidebarComponent {
+
+  search: string = "";
+  menus = Menus;
 
 }
