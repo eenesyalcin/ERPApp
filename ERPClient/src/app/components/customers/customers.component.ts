79162c2c66cc133c209acb112ef791d3cde4084d
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-08-14 17:48:20 +0300

    Create customer operations were carried out for the frontend.

--- ERPClient/src/app/components/customers/customers.component.ts
+++ ERPClient/src/app/components/customers/customers.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { SharedModule } from '../../modules/shared.module';
 import { CustomerModel } from '../../models/customer.model';
 import { HttpService } from '../../services/http.service';
 import { CustomerPipe } from '../../pipes/customer.pipe';
+import { NgForm } from '@angular/forms';
+import { SwalService } from '../../services/swal.service';
 
 @Component({
   selector: 'app-customers',
@@ -16,8 +18,13 @@ export class CustomersComponent implements OnInit {
   customers: CustomerModel[] = [];
   search: string = "";
 
+  @ViewChild("createModalCloseBtn") createModalCloseBtn: ElementRef<HTMLButtonElement> | undefined;
+  createModel: CustomerModel = new CustomerModel();
+  updateModel: CustomerModel = new CustomerModel();
+
   constructor(
-    private http: HttpService
+    private http: HttpService,
+    private swal: SwalService
   ) {}
 
   ngOnInit(): void {
@@ -28,6 +35,16 @@ export class CustomersComponent implements OnInit {
     this.http.post<CustomerModel[]>("Customers/GetAll", {}, (res) => {
       this.customers = res;
     });
+  }
+
+  create(form: NgForm){
+    if (form.valid) {
+      this.http.post<string>("Customer/Create", this.createModel, (res) => {
+        this.swal.callToast(res);
+        this.createModel = new CustomerModel();
+        this.createModalCloseBtn?.nativeElement.click();
+      });
+    }
   }
 
 }
