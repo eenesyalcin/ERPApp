ddfa0fc34b005c7affb077954b7256062078c412
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-08-15 15:03:27 +0300

    Customer update operations were performed in the backend and corrections were made in the frontend.

---
+++ ERPServer/ERPServer.Application/Features/Customers/UpdateCustomer/UpdateCustomerCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace ERPServer.Application.Features.Customers.UpdateCustomer;
+
+public sealed class UpdateCustomerCommandValidator : AbstractValidator<UpdateCustomerCommand>
+{
+    public UpdateCustomerCommandValidator()
+    {
+        RuleFor(p => p.Name).MinimumLength(3);
+        RuleFor(p => p.TaxDepartment).MinimumLength(3);
+        RuleFor(p => p.TaxNumber).MinimumLength(10).MaximumLength(11);
+        RuleFor(p => p.City).MinimumLength(3);
+        RuleFor(p => p.Town).MinimumLength(3);
+        RuleFor(p => p.FullAddress).MinimumLength(3);
+    }
+}
