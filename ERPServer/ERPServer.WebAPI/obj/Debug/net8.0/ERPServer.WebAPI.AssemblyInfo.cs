809e8cc83a660ebf0db5213f516f2f53efc20ba7
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-08-19 14:47:39 +0300

    Recipe CRUD operations were performed for the backend.

--- ERPServer/ERPServer.WebAPI/obj/Debug/net8.0/ERPServer.WebAPI.AssemblyInfo.cs
+++ ERPServer/ERPServer.WebAPI/obj/Debug/net8.0/ERPServer.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPServer.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c87a54f773bb58faebcbbf6ceb48e9f407d2767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69220742f540f6eb28664b200d4ac9147596ffb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPServer.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPServer.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
