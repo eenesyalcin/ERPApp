142dfc999578d2d3e414cd36998fa332bfc4ff8f
Enes YALÇIN <eenes.ylcn@gmail.com>
2024-08-22 12:43:23 +0300

    Invoice CRUD operations were performed for the backend.

--- ERPServer/ERPServer.Application/Mapping/MappingProfile.cs
+++ ERPServer/ERPServer.Application/Mapping/MappingProfile.cs
@@ -3,6 +3,7 @@ using ERPServer.Application.Features.Customers.CreateCustomer;
 using ERPServer.Application.Features.Customers.UpdateCustomer;
 using ERPServer.Application.Features.Depots.CreateDepot;
 using ERPServer.Application.Features.Depots.UpdateDepot;
+using ERPServer.Application.Features.Invoices.CreateInvoice;
 using ERPServer.Application.Features.Orders.CreateOrder;
 using ERPServer.Application.Features.Orders.UpdateOrder;
 using ERPServer.Application.Features.Products.CreateProduct;
@@ -44,6 +45,17 @@ namespace ERPServer.Application.Mapping
                     Quantity = s.Quantity,
                 }).ToList()));
             CreateMap<UpdateOrderCommand, Order>().ForMember(member => member.Details, options => options.Ignore());
+
+            CreateMap<CreateInvoiceCommand, Invoice>()
+                .ForMember(member => member.Type, options => options.MapFrom(P => InvoiceTypeEnum.FromValue(P.Type)))
+                .ForMember(member => member.Details,
+                options =>
+                options.MapFrom(p => p.Details.Select(s => new OrderDetail
+                {
+                    Price = s.Price,
+                    ProductId = s.ProductId,
+                    Quantity = s.Quantity,
+                }).ToList()));
         }
     }
 }
--- ERPServer/ERPServer.WebAPI/obj/Debug/net8.0/ERPServer.WebAPI.AssemblyInfo.cs
+++ ERPServer/ERPServer.WebAPI/obj/Debug/net8.0/ERPServer.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERPServer.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33b96565f52a5319fd27bb90b03f7d74892d692f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb2dbaea1147d249582b9fe29a8f434aef23ac56")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERPServer.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ERPServer.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
